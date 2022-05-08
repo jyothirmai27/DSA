034e267c1419ea410257a08ec510b21797599f7a
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-05-08 10:11:21 +0530

    Create Iterative.cpp

---
+++ Arrays/Reverse an Array/Iterative.cpp
@@ -0,0 +1,30 @@
+# iterative solution (Time Complexity - O(N))
+
+#include<bits/stdc++.h>
+using namespace std;
+void reverseArr(int arr[],int n)
+{
+    int i=0,j=n-1;
+    while(i<=j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    reverseArr(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
